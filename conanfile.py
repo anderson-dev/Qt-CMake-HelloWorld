64b73a9835204feda45a7e1d75ed9da53db166e1
Anderson Florence <aflorence@xenotran.com>
2022-01-04 18:01:10 -0500

    try import qt via Conan

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+from conans import ConanFile, MSBuild, tools
+
+class HelloWorld(ConanFile):
+     name = "HelloWorld"
+     license = "BSD"
+     author = "your name"
+     url = "git@gitlab.com:<url/project>"
+     description = "Base Library A of the example project."
+     topics = ("<Put some tag here>", "<here>", "<and here>")
+     generators = "cmake"
+    #  generators = "qt", "cmake", "cmake_find_package_multi", "qmake"
+
+     def build_requirements(self):
+         self.build_requires("gtest/1.8.1")
+
+     def requirements(self):
+         self.requires("qt/5.15.2")
+
+        # def package_info(self):
+        #     self.cpp_info.libs = ["BaseLibBLibrary"]
