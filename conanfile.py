653d3de17856a8e04b036177ebfca8fe16d33b4e
Anderson Florence <aflorence@xenotran.com>
2022-01-05 12:43:51 -0500

    corrected indentation

--- conanfile.py
+++ conanfile.py
@@ -1,20 +1,20 @@
-from conans import ConanFile, MSBuild, tools
+from conans import ConanFile, tools
 
 class HelloWorld(ConanFile):
-     name = "HelloWorld"
-     license = "BSD"
-     author = "your name"
-     url = "git@gitlab.com:<url/project>"
-     description = "Base Library A of the example project."
-     topics = ("<Put some tag here>", "<here>", "<and here>")
-     settings = "os", "arch", "compiler", "build_type"
-     generators = "cmake_find_package_multi"
+    name = "HelloWorld"
+    license = "BSD"
+    author = "your name"
+    url = "git@gitlab.com:<url/project>"
+    description = "Base Library A of the example project."
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake_find_package_multi"
 
-     def build_requirements(self):
-         self.build_requires("gtest/1.8.1")
+    def build_requirements(self):
+        self.build_requires("gtest/1.8.1")
 
-     def requirements(self):
-         self.requires("qt/5.15.2")
+    def requirements(self):
+        self.requires("qt/5.15.2")
 
-        # def package_info(self):
-        #     self.cpp_info.libs = ["BaseLibBLibrary"]
+    # def package_info(self):
+    #     self.cpp_info.libs = ["BaseLibBLibrary"]
