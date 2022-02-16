2010cd2d04508e6987124faadd5a3510e5159343
Anderson Florence <aflorence@xenotran.com>
2022-02-16 18:27:41 -0500

    corrected indentation

--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,25 @@
 from conans import ConanFile, MSBuild, tools
 
 class HelloWorld(ConanFile):
-     name = "HelloWorld"
-     license = "BSD"
-     author = "your name"
-     url = "git@gitlab.com:<url/project>"
-     description = "Base Library A of the example project."
-     topics = ("<Put some tag here>", "<here>", "<and here>")
-     settings = "os", "arch", "compiler", "build_type"
-     generators = "cmake_find_package_multi"
-     default_options = "qt:shared=true"
+    name = "HelloWorld"
+    license = "BSD"
+    author = "your name"
+    url = "git@gitlab.com:<url/project>"
+    description = "Base Library A of the example project."
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake_find_package_multi"
+    default_options = {"qt:shared": True}
 
-     def build_requirements(self):
-         self.build_requires("gtest/1.8.1")
+    def build_requirements(self):
+        self.build_requires("gtest/1.8.1")
 
-     def requirements(self):
-         self.requires("qt/5.15.2")
+    def requirements(self):
+        self.requires("qt/5.15.2")
 
-        # def package_info(self):
-        #     self.cpp_info.libs = ["BaseLibBLibrary"]
+    def imports(self):
+        self.copy("*.dll", "", "bin")
+        self.copy("*.dylib", "", "lib")
+    
+    # def package_info(self):
+    #     self.cpp_info.libs = ["BaseLibBLibrary"]
